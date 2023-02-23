f2c1403b0d34464000461cc42cc96bcd0a6b90bb
erik lima <erik.lima.f@outlook.com>
2023-02-23 18:54:16 -0300

    fix: :art: Mudei as nomenclaturas dentro das migrations

--- src/database/seeders/20230223051241-users.js
+++ src/database/seeders/20230223051241-users.js
@@ -1,28 +1,20 @@
-'use strict';
-
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-
-
     await queryInterface.bulkInsert('Users', [{
       id: 1,
       name: 'John Doe',
       email: 'john@gmail.com',
       password: '12345678',
-      logradouro: 'Rua Manoel Dias',
-      numberHome: 58,
-      cep: 07155712,
+      street: 'Rua Manoel Dias',
+      houseNumber: 58,
+      zipCode: 07155712,
     }], {
-      timestemps: false
+      timestamps: false
     });
-
   },
 
   async down(queryInterface, Sequelize) {
-
-
-    await queryInterface.bulkDelete('People', null, {});
-
+    await queryInterface.bulkDelete('Users', null, {});
   }
 };
