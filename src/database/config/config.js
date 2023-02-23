c75c320866090960ab57d5e04dee04305bdba89f
erik lima <erik.lima.f@outlook.com>
2023-02-23 18:51:07 -0300

    fix: :bug: Adicionando verificações
    A falta de uma condição não permitia a execução de tarefaz no banco

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 
 const config = {
-  username: process.env.POSTGRES_USER,
-  password: 'postgres',
+  database: process.env.POSTGRES_DB || 'db',
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
+  host: process.env.POSTGRES_HOST || 'localhost',
   dialect: 'postgres',
+  logging: false,
 }
 
 module.exports = config;
