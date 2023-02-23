1c8ade6d8c2c3853246a20dd191793606a3f7b60
erik lima <erik.lima.f@outlook.com>
2023-02-23 00:43:52 -0300

    feat: :sparkles: Adding error middleware

---
+++ src/Middleware/middleware.error.js
@@ -0,0 +1,10 @@
+const errorMiddleware = async (err, _req, res, next) => {
+  const { status, message } = err;
+
+  res.status(status).send({ message });
+
+  next();
+};
+
+
+module.exports = errorMiddleware;
--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 require('express-async-errors');
 const express = require('express');
 const cors = require('cors');
+const errorMiddleware = require('./Middleware/middleware.error');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(errorMiddleware);
 
 
 module.exports = app;
