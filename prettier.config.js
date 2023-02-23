e69c6d3edc7dc3bb6b45fedb361673a32bf478c3
erik lima <erik.lima.f@outlook.com>
2023-02-22 23:36:48 -0300

    build: :tada: Iniciando projeto
    Subindo a configuração inicial do projeto
    
    Adicionei as dependencias necessarias e configurei o servidor para progresso no desenvolvimento

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: true,
+  arrowParens: "always",
+  trailingComma: "es5",
+  semi: true
+};
---
+++ src/database/config/config.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const config = {
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+  dialect: 'postgres',
+}
+
+module.exports = config;
